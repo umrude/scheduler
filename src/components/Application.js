e7bfbb886a442b33b4d069ebf41ff687038af5a1
Gabi Geollegue <gabigeollegue@gmail.com>
2019-10-28 19:19:04 -0700

    add appointment components to application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-
+import { Fragment } from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
+import Appointment from "components/Appointment";
 
 const days = [
   {
@@ -21,10 +22,72 @@ const days = [
     spots: 0,
   },
 ];
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "3pm",
+    interview: {
+      student: "Gabi Geollegue",
+      interviewer: {
+        id: 1,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "4pm",
+    interview: {
+      student: "Test User 3",
+      interviewer: {
+        id: 1,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "2pm",
+    interview: {
+      student: "Test User 4",
+      interviewer: {
+        id: 1,
+        name: "Sven Jones",
+        avatar: "https://i.imgur.com/twYrpay.jpg",
+      }
+    }
+  }
+];
+
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
-
+  const allAppointments = appointments.map(appointment => {
+    return (
+      <Appointment
+        key={appointment.id}
+        {...appointment}
+      />
+    );
+  });
   return (
     <main className="layout">
       <section className="sidebar">
@@ -48,7 +111,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {allAppointments}
+    <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
