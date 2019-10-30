989577dc4a0f36856496cdae06ea5063e36d1be4
Gabi Geollegue <gabigeollegue@gmail.com>
2019-10-29 19:56:31 -0700

    retrieve appointments from API; add helpers with selectors

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,86 +3,46 @@ import React, { useState, useEffect } from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
+import {getAppointmentsForDay,getInterview} from "helpers/selectors";
 const axios = require('axios').default;
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "3pm",
-    interview: {
-      student: "Gabi Geollegue",
-      interviewer: {
-        id: 1,
-        name: "Tori Malcolm",
-        avatar: "https://i.imgur.com/Nmx0Qxo.png",
-      }
-    }
-  },
-  {
-    id: 4,
-    time: "4pm",
-    interview: {
-      student: "Test User 3",
-      interviewer: {
-        id: 1,
-        name: "Mildred Nazir",
-        avatar: "https://i.imgur.com/T2WwVfS.png",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "2pm",
-    interview: {
-      student: "Test User 4",
-      interviewer: {
-        id: 1,
-        name: "Sven Jones",
-        avatar: "https://i.imgur.com/twYrpay.jpg",
-      }
-    }
-  }
-];
+export default function Application() {
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
+  const setDay = day => setState({...state, day})
 
+  const allAppointments = getAppointmentsForDay(state, state.day);
 
-export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
-  const allAppointments = appointments.map(appointment => {
+  const schedule = allAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+
     return (
       <Appointment
         key={appointment.id}
-        {...appointment}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
       />
     );
   });
 
   useEffect(() => {
-    axios.get('/api/days')
-      .then(function (response) {
-        // handle success
-        setDays(response.data);
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      })
+    Promise.all([
+      Promise.resolve(axios.get('/api/days')),
+      Promise.resolve(axios.get('/api/appointments')),
+      Promise.resolve(axios.get('/api/interviewers'))
+
+    ]).then((data) => {
+      setState(() => ({ 
+        day:"Monday", 
+        days: data[0].data, 
+        appointments: data[1].data, 
+        interviewers: data[2].data}));
+    });
   }, []);
   return (
     <main className="layout">
@@ -95,9 +55,9 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList 
-          days={days} 
-          day={day} 
-          setDay={setDay} 
+          days={state.days} 
+          day={state.day} 
+          setDay={setDay}
           />
         </nav>
         <img
@@ -107,7 +67,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {allAppointments}
+        {schedule}
     <Appointment key="last" time="5pm" />
       </section>
     </main>
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -12,3 +12,17 @@ export function getAppointmentsForDay(state, day) {
   });
   return result;
 }
+
+
+
+export function getInterview(state, interview) {
+  if (interview === null) {
+    return null;
+  } 
+  const interviews = {
+    student: interview.student,
+    interviewer: state.interviewers[interview.interviewer]
+  };
+  
+  return interviews;
+}
