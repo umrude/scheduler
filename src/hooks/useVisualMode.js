89f7758e74d09135d3adbdbbbb0f73987df4de7f
Gabi Geollegue <gabigeollegue@gmail.com>
2019-10-30 13:54:54 -0700

    add custom hook to see current visual mode

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+
+
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+
+  function transition(mode, replace = false) {
+    setMode(mode);
+    setHistory(prev => [...prev, mode]); 
+
+    if (replace === true) {
+      setHistory(history.splice((history.length - 2), 1, mode))
+    }
+    
+  }
+
+  function back() {
+    if (history.length > 1){
+      history.pop();
+    }
+    setMode(history[history.length - 1]);
+  }
+
+  return { mode, transition, back };
+}
